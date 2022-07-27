21e2bc4601103707baa5eb143d88fb24905b2ba7
chrissolutions <chris@chrissolutions.com>
2022-07-27 13:50:55 +1200

    Fixed Block tests.

--- BsvSharp/CafeLib.BsvSharp.UnitTests/Chain/KzBlockTests.cs
+++ BsvSharp/CafeLib.BsvSharp.UnitTests/Chain/KzBlockTests.cs
@@ -71,7 +71,7 @@ namespace CafeLib.BsvSharp.UnitTests.Chain
             Assert.True(tx.Inputs[0].SequenceNumber == 4294967295U); // -1
             Assert.True(tx.Inputs[0].PrevOut.Index == -1);
             Assert.True(tx.Inputs[0].PrevOut.TxId.ToString() == "0000000000000000000000000000000000000000000000000000000000000000");
-            Assert.True(tx.Inputs[0].UtxoScript.ToHexString() == "04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73"); 
+            Assert.True(tx.Inputs[0].ScriptSig.ToHexString() == "04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73"); 
             Assert.True(tx.Outputs.Length == 1);
             Assert.True(tx.Outputs[0].Amount == 5000000000L);
             Assert.True(tx.Outputs[0].Script.ToHexString() == "4104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac");
--- BsvSharp/CafeLib.BsvSharp.UnitTests/Chain/KzNewBlockTests.cs
+++ BsvSharp/CafeLib.BsvSharp.UnitTests/Chain/KzNewBlockTests.cs
@@ -73,7 +73,7 @@ namespace CafeLib.BsvSharp.UnitTests.Chain
             Assert.True(tx.Inputs[0].PrevOut.Index == -1);
             Assert.True(tx.Inputs[0].PrevOut.TxId.ToString() ==
                         "0000000000000000000000000000000000000000000000000000000000000000");
-            Assert.True(tx.Inputs[0].UtxoScript.ToHexString() ==
+            Assert.True(tx.Inputs[0].ScriptSig.ToHexString() ==
                         "04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73");
             Assert.True(tx.Outputs.Count == 1);
             Assert.True(tx.Outputs[0].Amount == 5000000000L);
