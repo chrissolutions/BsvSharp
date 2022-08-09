ad70a04172659a429f4e12a68efcd63ee5d040e1
chrissolutions <chris@chrissolutions.com>
2022-08-09 16:51:59 +1200

    Updated PublicKey_From_PrivateKey_Test in KzPrivateKeyTests.cs

--- BsvSharp/CafeLib.BsvSharp.UnitTests/Keys/KzPrivateKeyTests.cs
+++ BsvSharp/CafeLib.BsvSharp.UnitTests/Keys/KzPrivateKeyTests.cs
@@ -54,12 +54,16 @@ namespace CafeLib.BsvSharp.UnitTests.Keys
             const string hex = "906977a061af29276e40bf377042ffbde414e496ae2260bbf1fa9d085637bfff";
             const string wif = "L24Rq5hPWMexw5mQi7tchYw6mhtr5ApiHZMN8KJXCkskEv7bTV61";
             const string publicKeyAddress = "17JarKo61PkpuZG3GyofzGmFSCskGRBUT3";
+            const string pubHex = "02a1633cafcc01ebfb6d78e39f687a1f0995c62fc95f51ead10a02ee0be551b5dc";
 
             var key1 = new PrivateKey(hex);
             var key2 = PrivateKey.FromWif(wif);
 
             var pubKey1 = key1.CreatePublicKey();
             var pubKey2 = key2.CreatePublicKey();
+
+            Assert.Equal(pubHex, pubKey1.ToHex());
+            Assert.Equal(pubHex, pubKey2.ToHex());
 
             Assert.Equal(pubKey1, pubKey2);
             Assert.Equal(publicKeyAddress, pubKey1.ToAddress().ToString());
