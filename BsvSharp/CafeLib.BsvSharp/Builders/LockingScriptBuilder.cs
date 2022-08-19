4d86a914a357e82bfee979a9fa03a5a72aa94c99
chrissolutions <chris@chrissolutions.com>
2022-08-20 09:19:38 +1200

    Minor syntax change in LockingScriptBuilder

--- BsvSharp/CafeLib.BsvSharp/Builders/LockingScriptBuilder.cs
+++ BsvSharp/CafeLib.BsvSharp/Builders/LockingScriptBuilder.cs
@@ -7,7 +7,7 @@ namespace CafeLib.BsvSharp.Builders
     public class LockingScriptBuilder : ScriptBuilder
     {
         public UInt160 PubKeyHash { get; }
-        
+
         public LockingScriptBuilder(Address address, TemplateId templateId = TemplateId.Unknown)
             : this(address.PubKeyHash, templateId)
         {
@@ -19,7 +19,7 @@ namespace CafeLib.BsvSharp.Builders
         }
 
         protected LockingScriptBuilder(UInt160 pubKeyHash, TemplateId templateId)
-            :base(true, templateId)
+            : base(true, templateId)
         {
             PubKeyHash = pubKeyHash;
         }
