64b0684f801f1233fd4b257a5097560cfa9305b9
chrissolutions <chris@chrissolutions.com>
2022-09-20 09:26:03 +1200

    Added BitcoinUnitTests.cs

---
+++ BsvSharp/CafeLib.BsvSharp.UnitTests/Units/BitcoinUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using CafeLib.BsvSharp.Units;
+using Xunit;
+
+namespace CafeLib.BsvSharp.UnitTests.Units
+{
+    public class BitcoinUnitTests
+    {
+        [Theory]
+        [InlineData(1000000L, 1000000, BitcoinUnit.Satoshi)]
+        [InlineData(1000000L, 10000, BitcoinUnit.Bit)]
+        [InlineData(1000000L, 2000, BitcoinUnit.Duro)]
+        [InlineData(1000000L, 10, BitcoinUnit.MilliBitcoin)]
+        [InlineData(1000000L, .01, BitcoinUnit.Bitcoin)]
+        public void Amount_Unit_Tests(long expectedAmount, decimal amountInUnit, BitcoinUnit unit)
+        {
+            var amount = new Amount(amountInUnit, unit);
+            Assert.Equal(expectedAmount, (long)amount);
+        }
+    }
+}
