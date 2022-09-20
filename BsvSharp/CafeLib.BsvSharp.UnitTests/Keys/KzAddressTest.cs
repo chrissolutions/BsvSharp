27fb46eab7fd97aa9758febd393f58c6c1da09a5
chrissolutions <chris@chrissolutions.com>
2022-09-21 11:57:39 +1200

    Added AddressFromBase58_Tests to KzAddressTest

--- BsvSharp/CafeLib.BsvSharp.UnitTests/Keys/KzAddressTest.cs
+++ BsvSharp/CafeLib.BsvSharp.UnitTests/Keys/KzAddressTest.cs
@@ -36,7 +36,32 @@ namespace CafeLib.BsvSharp.UnitTests.Keys
             Assert.Equal(addressType, address.AddressType);
             Assert.Equal(networkType, address.NetworkType);
         }
-        
+
+        [Theory]
+        [InlineData("13k3vneZ3yvZnc9dNWYH2RJRFsagTfAERv", AddressType.PublicKeyHash, NetworkType.Main)]
+        [InlineData("15vkcKf7gB23wLAnZLmbVuMiiVDc1Nm4a2", AddressType.PublicKeyHash, NetworkType.Main)]
+        [InlineData("1A6ut1tWnUq1SEQLMr4ttDh24wcbJ5o9TT", AddressType.PublicKeyHash, NetworkType.Main)]
+        [InlineData("1BpbpfLdY7oBS9gK7aDXgvMgr1DPvNhEB2", AddressType.PublicKeyHash, NetworkType.Main)]
+        [InlineData("1Jz2yCRd5ST1p2gUqFB5wsSQfdm3jaFfg7", AddressType.PublicKeyHash, NetworkType.Main)]
+        [InlineData("n28S35tqEMbt6vNad7A5K3mZ7vdn8dZ86X", AddressType.PublicKeyHash, NetworkType.Test)]
+        [InlineData("n45x3R2w2jaSC62BMa9MeJCd3TXxgvDEmm", AddressType.PublicKeyHash, NetworkType.Test)]
+        [InlineData("mursDVxqNQmmwWHACpM9VHwVVSfTddGsEM", AddressType.PublicKeyHash, NetworkType.Test)]
+        [InlineData("mtX8nPZZdJ8d3QNLRJ1oJTiEi26Sj6LQXS", AddressType.PublicKeyHash, NetworkType.Test)]
+        [InlineData("342ftSRCvFHfCeFFBuz4xwbeqnDw6BGUey", AddressType.ScriptHash, NetworkType.Main)]
+        [InlineData("33vt8ViH5jsr115AGkW6cEmEz9MpvJSwDk", AddressType.ScriptHash, NetworkType.Main)]
+        [InlineData("37Sp6Rv3y4kVd1nQ1JV5pfqXccHNyZm1x3", AddressType.ScriptHash, NetworkType.Main)]
+        [InlineData("3QjYXhTkvuj8qPaXHTTWb5wjXhdsLAAWVy", AddressType.ScriptHash, NetworkType.Main)]
+        [InlineData("mszYqVnqKoQx4jcTdJXxwKAissE3Jbrrc1", AddressType.PublicKeyHash, NetworkType.Test)]
+        [InlineData("mrU9pEmAx26HcbKVrABvgL7AwA5fjNFoDc", AddressType.PublicKeyHash, NetworkType.Test)]
+        [InlineData("mgBCJAsvzgT2qNNeXsoECg2uPKrUsZ76up", AddressType.PublicKeyHash, NetworkType.Test)]
+        public void AddressFromBase58_Tests(string base58, AddressType addressType, NetworkType networkType)
+        {
+            var address = Address.FromBase58(base58);
+            Assert.Equal(base58, address.ToString());
+            Assert.Equal(addressType, address.AddressType);
+            Assert.Equal(networkType, address.NetworkType);
+        }
+
         [Theory]
         [InlineData("15vkcKf7gB23wLAnZLmbVuMiiVDc3nq4a2")]
         [InlineData("1A6ut1tWnUq1SEQLMr4ttDh24wcbj4w2TT")]
