176a5029a113c7d683bd191f4f5c6aeba57fe617
chrissolutions <chris@chrissolutions.com>
2022-09-23 08:40:45 +1200

    Added Token_Clear_Amount_Test

--- BsvSharp/CafeLib.BsvSharp.UnitTests/Units/TokenTests.cs
+++ BsvSharp/CafeLib.BsvSharp.UnitTests/Units/TokenTests.cs
@@ -9,7 +9,7 @@ namespace CafeLib.BsvSharp.UnitTests.Units
         [InlineData(ExchangeUnit.USD, 50, 10, 500)]
         [InlineData(ExchangeUnit.BTC, .0025, 400, 1)]
         [InlineData(ExchangeUnit.BTC, .0025, 500, 1.25)]
-        public void Token_Test(ExchangeUnit foreign, decimal rate, decimal tokenQuantity, decimal bitcoin)
+        public void Token_Calculate_Amount_Test(ExchangeUnit foreign, decimal rate, decimal tokenQuantity, decimal bitcoin)
         {
             var token = new Token();
             var exchangeRate = new BsvExchangeRate(foreign, rate);
@@ -18,6 +18,16 @@ namespace CafeLib.BsvSharp.UnitTests.Units
             token.SetTokenQuantity(tokenQuantity);
 
             Assert.Equal(bitcoin, token.Amount.ToBitcoin()) ;
+        }
+
+        [Fact]
+        public void Token_Clear_Amount_Test()
+        {
+            var token = new Token(new Amount(100, BitcoinUnit.Bitcoin));
+            Assert.True(token.HasAmount);
+            Assert.Equal(100, token.Amount.ToBitcoin());
+            token.ClearAmount();
+            Assert.False(token.HasAmount);
         }
     }
 }
