5f7146a729457c7fac7403a706dde987f4cdffe8
chrissolutions <chris@chrissolutions.com>
2022-10-01 22:17:03 +1300

    Added Network_Type_Test

---
+++ BsvSharp/CafeLib.BsvSharp.UnitTests/Network/NetworkTests.cs
@@ -0,0 +1,26 @@
+﻿using CafeLib.BsvSharp.Builders;
+using CafeLib.BsvSharp.Extensions;
+using CafeLib.BsvSharp.Keys;
+using CafeLib.BsvSharp.Network;
+using CafeLib.BsvSharp.Scripting;
+using CafeLib.BsvSharp.Services;
+using CafeLib.BsvSharp.Signatures;
+using CafeLib.BsvSharp.Transactions;
+using CafeLib.Core.Numerics;
+using Xunit;
+
+namespace CafeLib.BsvSharp.UnitTests.Network
+{
+    public class NetworkTests
+    {
+        [Fact]
+        public void Network_Type_Test()
+        {
+            var networkMain = RootService.GetNetwork(NetworkType.Main);
+            Assert.Equal(NetworkType.Main, networkMain.NodeType);
+
+            var networkTest = RootService.GetNetwork(NetworkType.Test);
+            Assert.Equal(NetworkType.Test, networkTest.NodeType);
+        }
+    }
+}
