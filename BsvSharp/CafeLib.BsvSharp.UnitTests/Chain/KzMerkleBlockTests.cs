08c4232e5c297cbfc21f69bed3405bb898d8eb76
chrissolutions <chris@chrissolutions.com>
2022-10-21 16:23:49 +1300

    Changed MerkleBlock_FromJson_Filtered_Test into a Theory.  Added test against MainnetFilteredBlock399775

--- BsvSharp/CafeLib.BsvSharp.UnitTests/Chain/KzMerkleBlockTests.cs
+++ BsvSharp/CafeLib.BsvSharp.UnitTests/Chain/KzMerkleBlockTests.cs
@@ -19,11 +19,13 @@ namespace CafeLib.BsvSharp.UnitTests.Chain
             Assert.Equal("000000000000b731f2eef9e8c63173adfb07e41bd53eb0ef0a6b720d6cb6dea4", merkleBlock.Hash.ToString());
         }
 
-        [Fact]
-        public void MerkleBlock_FromJson_Filtered_Test()
+        [Theory]
+        [InlineData(MainnetBlock100014, "019f5b01d4195ecbc9398fbf3c3b1fa9bb3183301d7a1fb3bd174fcfa40a2b65")]
+        [InlineData(MainnetFilteredBlock399775, "6f64fd5aa9dd01f74c03656d376625cf80328d83d9afebe60cc68b8f0e245bd9")]
+        public void MerkleBlock_FromJson_Filtered_Test(string merkleJson, string filteredHash)
         {
-            var merkleBlock = MerkleBlock.FromJson(MainnetBlock100014);
-            var hashOfFilteredTx = UInt256.FromHex("019f5b01d4195ecbc9398fbf3c3b1fa9bb3183301d7a1fb3bd174fcfa40a2b65");
+            var merkleBlock = MerkleBlock.FromJson(merkleJson);
+            var hashOfFilteredTx = UInt256.FromHex(filteredHash);
             var filteredHashes = merkleBlock.FilteredTransactionHashes();
             Assert.Equal(hashOfFilteredTx, filteredHashes.First());
         }
